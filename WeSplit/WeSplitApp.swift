fba41d6c8b351fb57f2fe65c264093f97b51c67d
Tiago Baptista Noronha <tiago.bn@gmail.com>
2023-01-31 20:07:18 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Tiago Baptista Noronha on 15/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
